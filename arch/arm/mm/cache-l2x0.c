168359bf26e8cd93f5537ee54a4b22d0374efd9e
Emil Lauridsen <mine809@gmail.com>
2011-05-23 13:16:37 +0200

    Revert "arm: io: import writel/readl_relaxed (Pershoo)t"
    This reverts commit af9deb5974b9c6e6f59a771ef4514f993cb055c2.

--- arch/arm/mm/cache-l2x0.c
+++ arch/arm/mm/cache-l2x0.c
@@ -44,7 +44,7 @@ static inline void cache_wait(void __iomem *reg, unsigned long mask)
 static inline void cache_wait(void __iomem *reg, unsigned long mask)
 {
 	/* wait for the operation to complete */
-	while (readl_relaxed(reg) & mask)
+	while (readl(reg) & mask)
 		;
 }
 
@@ -60,14 +60,14 @@ static DEFINE_SPINLOCK(l2x0_lock);
 static inline void cache_wait_always(void __iomem *reg, unsigned long mask)
 {
 	/* wait for the operation to complete */
-	while (readl_relaxed(reg) & mask)
+	while (readl(reg) & mask)
 		;
 }
 
 static inline void cache_sync(void)
 {
 	void __iomem *base = l2x0_base;
-	writel_relaxed(0, base + L2X0_CACHE_SYNC);
+	writel(0, base + L2X0_CACHE_SYNC);
 	cache_wait(base + L2X0_CACHE_SYNC, 1);
 }
 
@@ -75,14 +75,14 @@ static inline void l2x0_clean_line(unsigned long addr)
 {
 	void __iomem *base = l2x0_base;
 	cache_wait(base + L2X0_CLEAN_LINE_PA, 1);
-	writel_relaxed(addr, base + L2X0_CLEAN_LINE_PA);
+	writel(addr, base + L2X0_CLEAN_LINE_PA);
 }
 
 static inline void l2x0_inv_line(unsigned long addr)
 {
 	void __iomem *base = l2x0_base;
 	cache_wait(base + L2X0_INV_LINE_PA, 1);
-	writel_relaxed(addr, base + L2X0_INV_LINE_PA);
+	writel(addr, base + L2X0_INV_LINE_PA);
 }
 
 #ifdef CONFIG_PL310_ERRATA_588369
@@ -103,9 +103,9 @@ static inline void l2x0_flush_line(unsigned long addr)
 
 	/* Clean by PA followed by Invalidate by PA */
 	cache_wait(base + L2X0_CLEAN_LINE_PA, 1);
-	writel_relaxed(addr, base + L2X0_CLEAN_LINE_PA);
+	writel(addr, base + L2X0_CLEAN_LINE_PA);
 	cache_wait(base + L2X0_INV_LINE_PA, 1);
-	writel_relaxed(addr, base + L2X0_INV_LINE_PA);
+	writel(addr, base + L2X0_INV_LINE_PA);
 }
 #else
 
@@ -118,7 +118,7 @@ static inline void l2x0_flush_line(unsigned long addr)
 {
 	void __iomem *base = l2x0_base;
 	cache_wait(base + L2X0_CLEAN_INV_LINE_PA, 1);
-	writel_relaxed(addr, base + L2X0_CLEAN_INV_LINE_PA);
+	writel(addr, base + L2X0_CLEAN_INV_LINE_PA);
 }
 #endif
 
@@ -137,7 +137,7 @@ static inline void l2x0_inv_all(void)
 
 	/* invalidate all ways */
 	l2x0_lock(&l2x0_lock, flags);
-	writel_relaxed(0xff, l2x0_base + L2X0_INV_WAY);
+	writel(0xff, l2x0_base + L2X0_INV_WAY);
 	cache_wait_always(l2x0_base + L2X0_INV_WAY, 0xff);
 	cache_sync();
 	l2x0_unlock(&l2x0_lock, flags);
@@ -149,7 +149,7 @@ static inline void l2x0_flush_all(void)
 
 	/* flush all ways */
 	l2x0_lock(&l2x0_lock, flags);
-	writel_relaxed(0xff, l2x0_base + L2X0_CLEAN_INV_WAY);
+	writel(0xff, l2x0_base + L2X0_CLEAN_INV_WAY);
 	cache_wait_always(l2x0_base + L2X0_CLEAN_INV_WAY, 0xff);
 	cache_sync();
 	l2x0_unlock(&l2x0_lock, flags);
@@ -289,19 +289,19 @@ static void l2x0_enable(__u32 aux_val, __u32 aux_mask)
 	 * If you are booting from non-secure mode
 	 * accessing the below registers will fault.
 	 */
-	if (!(readl_relaxed(l2x0_base + L2X0_CTRL) & 1)) {
+	if (!(readl(l2x0_base + L2X0_CTRL) & 1)) {
 
 		/* l2x0 controller is disabled */
 
-		aux = readl_relaxed(l2x0_base + L2X0_AUX_CTRL);
+		aux = readl(l2x0_base + L2X0_AUX_CTRL);
 		aux &= aux_mask;
 		aux |= aux_val;
-		writel_relaxed(aux, l2x0_base + L2X0_AUX_CTRL);
+		writel(aux, l2x0_base + L2X0_AUX_CTRL);
 
 		l2x0_inv_all();
 
 		/* enable L2X0 */
-		writel_relaxed(1, l2x0_base + L2X0_CTRL);
+		writel(1, l2x0_base + L2X0_CTRL);
 	}
 }
 
