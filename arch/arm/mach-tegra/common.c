c914cb46fe3f4f3fb77d93796cba316721b5f789
Benedikt Boss <vork.android@gmail.com>
2011-06-07 02:47:39 -0400

    tegra: common: change 0x7C080001 to 0x7C480001 in tegra_init_cache (from some nvidia 2.6.36 commit)

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -246,7 +246,7 @@ void __init tegra_init_cache(void)
 	writel(7, p + L2X0_PREFETCH_OFFSET);
 	writel(2, p + L2X0_PWR_CTRL);
 
-	l2x0_init(p, 0x7C080001, 0x8200c3fe);
+	l2x0_init(p, 0x7C480001, 0x8200c3fe);
 #endif
 }
 
